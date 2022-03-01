78ab2e982c37b49936b06d59c4117b8dc966acff
Jordan Pittman <jordan@cryptica.me>
2022-03-01 10:53:53 -0500

    Match typography plugin test setup

--- test/index.test.js
+++ test/index.test.js
@@ -1,32 +1,26 @@
-const cssMatcher = require('jest-matcher-css')
+const path = require('path')
 const postcss = require('postcss')
 const tailwindcss = require('tailwindcss')
+const lineClampPlugin = require('../src')
 
-const defaultOptions = {
-  corePlugins: { preflight: false },
-  plugins:  [require('../src')],
-}
+function run(config, plugin = tailwindcss) {
+  let { currentTestName } = expect.getState()
+  config = {
+    ...{ plugins: [lineClampPlugin], corePlugins: { preflight: false } },
+    ...config,
+  }
 
-function run(input, config, plugin = tailwindcss) {
-  return postcss(
-    plugin(config)
-  )
-  .process(input, {
-    from: undefined
+  return postcss(plugin(config)).process('@tailwind utilities', {
+    from: `${path.resolve(__filename)}?test=${currentTestName}`,
   })
 }
 
-expect.extend({
-  toMatchCss: cssMatcher
-})
-
 it('should add the `line-clamp-{n}` components', () => {
   const config = {
-    ...defaultOptions,
     content: [{ raw: String.raw`<div class="line-clamp-2 line-clamp-[33] line-clamp-[var(--line-clamp-variable)]"></div>` }],
   }
 
-  return run('@tailwind components;', config).then((result) => {
+  return run(config).then((result) => {
     expect(result.css).toMatchCss(String.raw`
       .line-clamp-2 {
         overflow: hidden;
@@ -54,11 +48,10 @@ it('should add the `line-clamp-{n}` components', () => {
 
 it('should add the `line-clamp-none` utility', () => {
   const config = {
-    ...defaultOptions,
     content: [{ raw: String.raw`<div class="line-clamp-none"></div>` }],
   }
 
-  return run('@tailwind utilities;', config).then((result) => {
+  return run(config).then((result) => {
     expect(result.css).toMatchCss(String.raw`
       .line-clamp-none {
         -webkit-line-clamp: unset;
